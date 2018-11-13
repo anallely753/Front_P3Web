76f0a28c9b713da4068b4f6024661c835a3936ee
anallely753 <anallely.0710@gmail.com>
2018-11-13 05:24:12 -0600

    jdf

--- js/app.js
+++ js/app.js
@@ -10,4 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
     indicators: true
   });
 
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
  
