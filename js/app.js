be82f3d136f4b9363513e06f14ec733a22b4cbea
anallely753 <anallely.0710@gmail.com>
2018-11-13 04:00:02 -0600

    index

---
+++ js/app.js
@@ -0,0 +1,13 @@
+ M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+ var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+ 
